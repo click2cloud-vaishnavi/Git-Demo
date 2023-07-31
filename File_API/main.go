3452841b2714d67b16ee8173b3458b8a08eedf83
click2cloud-vaishnavi <vaishnavi.nagardhankar@click2cloud.net>
2023-07-31 15:54:28 +0530

    Changes

--- File_API/main.go
+++ File_API/main.go
@@ -7,64 +7,10 @@ import (
 	router "github.com/vaishnavi/File_API/controller"
 )
 
-// type File struct {
-// 	Name string `json:"name"`
-// 	Data string `json:"data"`
-// }
-
 func main() {
 	fmt.Println("Welcome to the File")
 	r := router.Router()
 
-	// r.HandleFunc("/create", Create_File).Methods("POST")
-	// r.HandleFunc("/get/{name}", Get_File).Methods("GET")
-	// r.HandleFunc("/update", Update_File).Methods("PUT")
-	// r.HandleFunc("/delete/{name}", Delete_File).Methods("DELETE")
-
 	http.ListenAndServe(":5000", r)
 
 }
-
-// func Create_File(w http.ResponseWriter, r *http.Request) {
-
-// 	file := File{}
-// 	_ = json.NewDecoder(r.Body).Decode(&file)
-// 	name := file.Name
-// 	data := file.Data
-
-// 	_ = ioutil.WriteFile(name, []byte(data), 0644)
-
-// 	fmt.Fprintf(w, "File created successfully...!!!!!")
-// }
-
-// func Get_File(w http.ResponseWriter, r *http.Request) {
-// 	name := r.URL.Path[len("/get/"):]
-
-// 	data, err := ioutil.ReadFile(name)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	fmt.Fprintf(w, string(data))
-
-// }
-
-// func Update_File(w http.ResponseWriter, r *http.Request) {
-
-// 	file := File{}
-// 	_ = json.NewDecoder(r.Body).Decode(&file)
-
-// 	name := file.Name
-// 	data := file.Data
-// 	_ = ioutil.WriteFile(name, []byte(data), 0644)
-
-// 	fmt.Fprintf(w, "File updated successfully")
-
-// }
-
-// func Delete_File(w http.ResponseWriter, r *http.Request) {
-
-// 	name := r.URL.Path[len("/delete/"):]
-// 	_ = os.Remove(name)
-
-// 	fmt.Fprintf(w, "File deleted successfully")
-// }
