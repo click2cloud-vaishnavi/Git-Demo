e2bfc3f6dd4612aaf1b8cb7e78d4712fe3bf0336
click2cloud-vaishnavi <vaishnavi.nagardhankar@click2cloud.net>
2023-07-31 15:52:51 +0530

    Changes

--- File_API/main.go
+++ File_API/main.go
@@ -1,73 +1,70 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net/http"
-	"os"
 
-	"github.com/gorilla/mux"
+	router "github.com/vaishnavi/File_API/controller"
 )
 
-type File struct {
-	Name string `json:"name"`
-	Data string `json:"data"`
-}
+// type File struct {
+// 	Name string `json:"name"`
+// 	Data string `json:"data"`
+// }
 
 func main() {
 	fmt.Println("Welcome to the File")
-	r := mux.NewRouter()
+	r := router.Router()
 
-	r.HandleFunc("/create", Create_File).Methods("POST")
-	r.HandleFunc("/get/{name}", Get_File).Methods("GET")
-	r.HandleFunc("/update", Update_File).Methods("PUT")
-	r.HandleFunc("/delete/{name}", Delete_File).Methods("DELETE")
+	// r.HandleFunc("/create", Create_File).Methods("POST")
+	// r.HandleFunc("/get/{name}", Get_File).Methods("GET")
+	// r.HandleFunc("/update", Update_File).Methods("PUT")
+	// r.HandleFunc("/delete/{name}", Delete_File).Methods("DELETE")
 
 	http.ListenAndServe(":5000", r)
 
 }
 
-func Create_File(w http.ResponseWriter, r *http.Request) {
+// func Create_File(w http.ResponseWriter, r *http.Request) {
 
-	file := File{}
-	_ = json.NewDecoder(r.Body).Decode(&file)
-	name := file.Name
-	data := file.Data
+// 	file := File{}
+// 	_ = json.NewDecoder(r.Body).Decode(&file)
+// 	name := file.Name
+// 	data := file.Data
 
-	_ = ioutil.WriteFile(name, []byte(data), 0644)
+// 	_ = ioutil.WriteFile(name, []byte(data), 0644)
 
-	fmt.Fprintf(w, "File created successfully...!!!!!")
-}
+// 	fmt.Fprintf(w, "File created successfully...!!!!!")
+// }
 
-func Get_File(w http.ResponseWriter, r *http.Request) {
-	name := r.URL.Path[len("/get/"):]
+// func Get_File(w http.ResponseWriter, r *http.Request) {
+// 	name := r.URL.Path[len("/get/"):]
 
-	data, err := ioutil.ReadFile(name)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Fprintf(w, string(data))
+// 	data, err := ioutil.ReadFile(name)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	fmt.Fprintf(w, string(data))
 
-}
+// }
 
-func Update_File(w http.ResponseWriter, r *http.Request) {
+// func Update_File(w http.ResponseWriter, r *http.Request) {
 
-	file := File{}
-	_ = json.NewDecoder(r.Body).Decode(&file)
+// 	file := File{}
+// 	_ = json.NewDecoder(r.Body).Decode(&file)
 
-	name := file.Name
-	data := file.Data
-	_ = ioutil.WriteFile(name, []byte(data), 0644)
+// 	name := file.Name
+// 	data := file.Data
+// 	_ = ioutil.WriteFile(name, []byte(data), 0644)
 
-	fmt.Fprintf(w, "File updated successfully")
+// 	fmt.Fprintf(w, "File updated successfully")
 
-}
+// }
 
-func Delete_File(w http.ResponseWriter, r *http.Request) {
+// func Delete_File(w http.ResponseWriter, r *http.Request) {
 
-	name := r.URL.Path[len("/delete/"):]
-	_ = os.Remove(name)
+// 	name := r.URL.Path[len("/delete/"):]
+// 	_ = os.Remove(name)
 
-	fmt.Fprintf(w, "File deleted successfully")
-}
+// 	fmt.Fprintf(w, "File deleted successfully")
+// }
